9fa6657a08b4737f9f0f993c9e0f69fefb87e125
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-07-31 11:00:22 -0500

    Update vigenere_cipher_helper.py

--- oop/vigenere_cipher_helper.py
+++ oop/vigenere_cipher_helper.py
@@ -25,14 +25,15 @@ class VigenereCipher(object):
                                     
                 # c = ASCII value of c + how far key_ptr char is from a
                 result += chr(ord(c) + shift)
-                
-                # Increment the key pointer
-                if key_ptr == len(self.key) - 1:
-                    key_ptr = 0
-                else: 
-                    key_ptr += 1
+
             else:
                 result += c
+            
+            # Increment the key pointer
+            if key_ptr == len(self.key) - 1:
+                key_ptr = 0
+            else: 
+                key_ptr += 1
             
         return result
         
@@ -40,10 +41,32 @@ class VigenereCipher(object):
     def decode(self, text: str) -> str:
         """Takes an encoded string and returns a plaintext version"""
         
+        key_ptr = 0
         result = ""
-        
-        #
-        
+            
+        # Iterate over the string characters and create result
+        for c in text:
+                    
+            # Only convert letters in alphabet, per spec
+            if c in self.alphabet:
+                shift = self.displacements[self.key[key_ptr]]
+                
+                # Wrap the shift, if necessary
+                if ord(c) - shift < ord('a'):
+                    shift -= 26
+                                                        
+                # c = ASCII value of c + how far key_ptr char is from a
+                result += chr(ord(c) - shift)
+                
+            else:
+                result += c
+                
+            # Increment the key pointer
+            if key_ptr == len(self.key) - 1:
+                key_ptr = 0
+            else: 
+                key_ptr += 1
+            
         return result
     
     def createDisplacementDict(self, key: str) -> None:
