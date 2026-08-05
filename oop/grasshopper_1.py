6bf977bff524b87c637f0644b85bc9a564236bc2
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-08-05 18:07:55 -0500

    Create grasshopper_1.py

---
+++ oop/grasshopper_1.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55e8aba23d399a59500000ce
+
+class Hero:
+    def __init__(self, name: str = "Hero") -> None:
+        self.name = name
+        self.position = "00"
+        self.health = 100
+        self.damage = 5
+        self.experience = 0
