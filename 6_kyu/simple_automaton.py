37dc43bc3ebf2acdc7a8b20f40a462356788ec2f
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-08-06 10:06:31 -0500

    Create simple_automaton.py

---
+++ 6_kyu/simple_automaton.py
@@ -0,0 +1,36 @@
+# https://www.codewars.com/kata/5268acac0d3f019add000203
+
+class Automaton(object):
+    
+    # Captures automaton states as keys, accept state status as values
+    ACCEPT_STATE = "q2"
+    STATE_TRANSITIONS = {
+        "q1": {
+            "0": "q1",
+            "1": "q2"
+        },
+        "q2": {
+            "0": "q3",
+            "1": "q2"
+        },    
+        "q3": {
+            "0": "q2",
+            "1": "q2"
+        }
+    }
+
+    def __init__(self):
+        pass
+        
+    def read_commands(self, commands):
+        """
+        Accept or reject the commands based on whether we end in our accept state, q2
+        """
+        current_state = "q1"
+        
+        for symbol in commands:
+            current_state = self.STATE_TRANSITIONS[current_state][symbol]
+        
+        return current_state == self.ACCEPT_STATE
+
+my_automaton = Automaton()
