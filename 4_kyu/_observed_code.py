912cc1dd7b6ed7b9c7e0057487fd721e68b091aa
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-09-08 12:26:11 -0500

    Create _observed_code.py

---
+++ 4_kyu/_observed_code.py
@@ -0,0 +1,40 @@
+# https://www.codewars.com/kata/5263c6999e0f40dee200059d
+
+# unfinished
+
+POSSIBLE_NUMS = {
+    '1': ['1', '2', '4'],
+    '2': ['1', '2', '5', '3'],
+    '3': ['2', '3', '6'],
+    '4': ['1', '4', '5', '7'],
+    '5': ['2', '4', '5', '6', '8'],
+    '6': ['3', '5', '6', '9'],
+    '7': ['4', '7', '8'],
+    '8': ['0', '5', '7', '8', '9'],
+    '9': ['6', '8', '9'],
+    '0': ['0', '8'],
+}
+def get_pins(observed: str):
+    
+#     for idx in range(len(observed)):
+#         for adjacent in POSSIBLE_NUMS[observed[idx]]:
+#             print(f"idx: {idx}, possible nume: {adjacent} code: {observed[0:idx] + adjacent + observed[idx:len(observed)-1]}")
+#             code = observed[0:idx] + adjacent + observed[idx:len(observed)-1]
+#             possible_codes.append(code)
+
+    possible_codes = []
+    permute(observed, 0, possible_codes)
+
+    
+def permute(observed: str, idx: int, possible_codes: list[str])
+
+    # Base case: No more digits to permute
+    if idx == len(observed):
+        return set(possible_codes)
+
+    # Recursive case: Permute the current digit
+    permute(observed, idx + 1, possible_codes)
+    
+    # Recursive logic
+    for adjacent in POSSIBLE_NUMS[observed[idx]]:
+        
