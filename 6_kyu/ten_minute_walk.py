18b0dd765dff8378337bfcab8b7219302aa8469f
Lauren Escobedo <89123282+laurenesco@users.noreply.github.com>
2026-09-18 11:36:46 -0500

    Create ten_minute_walk.py

---
+++ 6_kyu/ten_minute_walk.py
@@ -0,0 +1,20 @@
+# https://www.codewars.com/kata/54da539698b8a2ad76000228
+
+def is_valid_walk(walk: list[str]) -> bool:
+    ns_position = 0
+    we_position = 0
+
+    for direction in walk:
+        match direction:
+            case 'n':
+                ns_position += 1 
+            case 's':
+                ns_position -= 1 
+            case 'w':
+                we_position -= 1 
+            case 'e':
+                we_position += 1 
+            case _:
+                raise Exception("Unknown direction provided")
+    
+    return len(walk) == 10 and ns_position == 0 and we_position == 0
